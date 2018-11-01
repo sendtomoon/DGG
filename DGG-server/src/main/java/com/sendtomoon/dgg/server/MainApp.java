1d788738addac45af8429828b63800257297a7fd
galaxysha512 <galaxysha512@gmail.com>
2018-11-02 07:54:07 +0800

    完成基础搭建

--- DGG-server/src/main/java/com/sendtomoon/dgg/server/MainApp.java
+++ DGG-server/src/main/java/com/sendtomoon/dgg/server/MainApp.java
@@ -4,11 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@MapperScan(basePackages="com.sendtomoon.dgg.server.dao")
+@MapperScan(basePackages = "com.sendtomoon.dgg.server.dao")
 @EnableAutoConfiguration
-public class MainApp {
+public class MainApp extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MainApp.class);
+	}
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MainApp.class, args);
 	}
