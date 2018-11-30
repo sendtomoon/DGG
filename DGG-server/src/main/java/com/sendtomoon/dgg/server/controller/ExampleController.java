cae19dc259b9a5da38eae6199798a1afb8e453bc
galaxysha512 <galaxysha512@gmail.com>
2018-11-30 15:02:49 +0800

    增加swagger

--- DGG-server/src/main/java/com/sendtomoon/dgg/server/controller/ExampleController.java
+++ DGG-server/src/main/java/com/sendtomoon/dgg/server/controller/ExampleController.java
@@ -3,34 +3,34 @@ package com.sendtomoon.dgg.server.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sendtomoon.dgg.server.base.BaseController;
 import com.sendtomoon.dgg.server.service.impl.ExampleServiceImpl;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @Controller(value = "exampleService")
-public class ExampleController extends BaseController{
+public class ExampleController extends BaseController {
 
 	@Autowired
 	private ExampleServiceImpl server;
 
-	@RequestMapping(value = "/ex1")
+	@ApiOperation(value = "测试1", httpMethod = "GET")
+	@RequestMapping(value = "/ex1", method = RequestMethod.GET)
 	@ResponseBody
-	public String ex1(@RequestParam(value = "name", required = true) String name) {
+	public String ex1(@ApiParam(value = "name") @RequestParam(value = "name", required = true) String name) {
 		return "name:" + name;
 	}
 
-	@RequestMapping(value = "/ex2")
+	@ApiOperation(value = "测试2", httpMethod = "GET")
+	@RequestMapping(value = "/ex2", method = RequestMethod.GET)
 	@ResponseBody
 	public String ex2() {
 		return server.insertId();
-	}
-	
-	@RequestMapping(value = "/success")
-	@ResponseBody
-	public String success() {
-		return "success";
 	}
 
 }
--- DGG-server/src/main/java/com/sendtomoon/dgg/server/controller/IPInfoController.java
+++ DGG-server/src/main/java/com/sendtomoon/dgg/server/controller/IPInfoController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,8 +23,8 @@ public class IPInfoController extends BaseController {
 	@Autowired
 	private IPInfoService iif;
 
-	@ApiOperation(value = "插入IP更新信息")
-	@RequestMapping(value = "/recviceipinfo")
+	@ApiOperation(value = "插入IP更新信息", httpMethod = "PUT")
+	@RequestMapping(value = "/recviceipinfo", method = RequestMethod.PUT)
 	@ResponseBody
 	public CommonVO recviceipinfo(
 			@ApiParam(value = "事件名称") @RequestParam(value = "eventName", required = true) String eventName,
